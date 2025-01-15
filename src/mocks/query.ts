59459376db3b51a62029c5984ce7a896f552af93
Stephen <stephenn649@gmail.com>
2025-01-15 17:09:11 -0600

    Finished adding tests for Select

--- src/mocks/query.ts
+++ src/mocks/query.ts
@@ -1,39 +1,39 @@
-import domTestingLib from '@testing-library/dom';
+import domTestingLib from "@testing-library/dom";
 const { queryHelpers } = domTestingLib;
 
 export const queryByQueryId = queryHelpers.queryByAttribute.bind(
   null,
-  'query-id',
-)
+  "query-id",
+);
 
 export const queryAllByQueryId = queryHelpers.queryAllByAttribute.bind(
   null,
-  'query-id',
-)
+  "query-id",
+);
 
 export function getAllByQueryId(id: string) {
   const container = document.body;
   const els = queryAllByQueryId(container, id);
   if (!els.length) {
     throw queryHelpers.getElementError(
-      `Unable to find an element by: [data-query-id="${id}"]`,
+      `Unable to find an element by: [query-id="${id}"]`,
       container,
-    )
+    );
   }
-  return els
+  return els;
 }
 
 export function getByQueryId(id: string) {
   // result >= 1
   const container = document.body;
-  const result = getAllByQueryId(id)
+  const result = getAllByQueryId(id);
   if (result.length > 1) {
     throw queryHelpers.getElementError(
-      `Found multiple elements with the [data-query-id="${id}"]`,
+      `Found multiple elements with the [query-id="${id}"]`,
       container,
-    )
+    );
   }
-  return result[0]
+  return result[0];
 }
 
 // re-export with overrides
@@ -43,4 +43,4 @@ module.exports = {
   getAllByQueryId,
   queryByQueryId,
   queryAllByQueryId,
-}
+};
